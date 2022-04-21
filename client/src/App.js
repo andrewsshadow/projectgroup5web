d596c0da6c010ed38303d78a0fed8e23bf7a9672
habiblawal1 <habiblawal@hotmail.co.uk>
2022-04-21 13:04:07 +0100

    Auth context

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Register from "./pages/Register";
+import AuthState from "./context/authContext/AuthState";
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
   return (
-    <Router>
-      <Register />
-    </Router>
+    <AuthState>
+      <Router>
+        <Route exact path="/" component={Register} />
+      </Router>
+    </AuthState>
   );
 }
 
